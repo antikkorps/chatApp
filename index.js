c0c73b1856eeb372f9e6376cc0a5fbfda3086e95
Franck <franckvienot7@gmail.com>
2024-06-08 18:04:45 +0200

    preparing persistance of messages

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ import { createServer } from "node:http"
 import { fileURLToPath } from "node:url"
 import { dirname, join } from "node:path"
 import { Server } from "socket.io"
+import sqlite3 from "sqlite3"
+import { open } from "sqlite"
+
+// open the database file
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+})
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`)
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {},
+})
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -16,6 +35,13 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected")
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg)
+    io.emit("chat message", msg)
+  })
+  socket.on("disconnect", () => {
+    console.log("user disconnected")
+  })
 })
 
 server.listen(3000, () => {
